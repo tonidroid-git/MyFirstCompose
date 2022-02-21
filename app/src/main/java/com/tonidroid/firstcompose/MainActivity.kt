ac11c7fc52fc54e80f5eecb720ae68590b34480c
toni <idroidtoni@gmail.com>
2022-02-22 01:07:23 +0300

    finish first compose codelab

--- app/src/main/java/com/tonidroid/firstcompose/MainActivity.kt
+++ app/src/main/java/com/tonidroid/firstcompose/MainActivity.kt
@@ -1,83 +1,166 @@
 package com.tonidroid.firstcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.tonidroid.firstcompose.ui.theme.FirstComposeTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             FirstComposeTheme {
-                ComposeApp()
+                var shouldShownWhenLoading by rememberSaveable { mutableStateOf(true) }
+
+                if (shouldShownWhenLoading) {
+                    OnboardingScreen(
+                        onContinueClicked = { shouldShownWhenLoading = false }
+                    )
+                } else {
+                    ComposeApp()
+                }
             }
         }
     }
 }
 
 @Composable
-fun ComposeApp(words: List<String> = listOf("Android", "Tonidroid")) {
-    // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Column(
-            modifier = Modifier.padding(vertical = 8.dp)
-        ) {
-            for (word in words) {
-                Greeting(name = word)
-            }
+fun ComposeApp() {
+    Greetings()
+}
+
+@Composable
+fun Greetings(words: List<String> = List(1000) { "#$it" }) {
+    LazyColumn(
+        modifier = Modifier.padding(vertical = 4.dp)
+    ) {
+        items(items = words) { word ->
+            Greeting(word = word)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+fun Greeting(word: String) {
 
-    val expanded = remember {
-        mutableStateOf(false)
-    }
-
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessMedium
+                    )
+                )
+        ) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = word,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+                if(expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4)
+                    )
+                }
             }
-            OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-            ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+
+                )
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     FirstComposeTheme {
         ComposeApp()
+    }
+}
+
+
+// THIS IS ONBOARDING SCREEN
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Welcome to toni basic compose",
+                modifier = Modifier.padding(start = 24.dp, end = 24.dp)
+            )
+            Text(
+                text = "(example from codelab)"
+            )
+            Button(
+                modifier = Modifier.padding(24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    FirstComposeTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
